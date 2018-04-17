e43d7908016367879dfd826182da39f5c9b8c810
x22x22 <rootshellexp@gmail.com>
2018-04-17 22:07:09 +0800

    add runkit support (#1)
    * add runkit support
    
    add runkit support
    
    * Update README.md
    
    * Update README.md

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ oEmbed tag plugin for Hexo.
 * Instagram
 * Gyazo
 * Codepen
+* RunKik
 
 ## Install
 ```
@@ -17,4 +18,39 @@ npm install hexo-tag-oembed --save
 ## Usage
 ```
 {%oembed [permalink] %}
+```
+
+## Embed RunKik
+
+Please add the following code in the footer section of the theme:
+
+My theme is next, so modify the _layout.swig file.
+
+```html
+<script>
+window.addEventListener('message', function(e) {
+  if (e.origin !== "https://runkit.com")
+    return;
+
+  try {
+    var data = JSON.parse(e.data);
+  } catch (e) {
+    return false;
+  }
+
+  if (data.context !== 'iframe.resize') {
+    return false;
+  }
+
+  var iframe = document.querySelector('iframe[src="' + data.src + '"]');
+
+  if (!iframe) {
+    return false;
+  }
+
+  if (data.height) {
+    iframe.height = data.height;
+  }
+});
+</script>
 ```
--- index.js
+++ index.js
@@ -3,62 +3,67 @@
 var requestpromise = require('request-promise');
 var querystring = require('querystring');
 
-hexo.extend.tag.register('oembed', function(args){
-  if(!args) return;
+hexo.extend.tag.register(
+  'oembed',
+  function(args) {
+    if (!args) return;
+    var opts = {
+      url: args[0],
+      format: 'json'
+    };
 
-  var opts = {
-    "url": args[0],
-    "format": "json"
-  };
+    var endpoint = '';
+    if (opts.url.match(/^http(s?):\/\/twitter\.com\/(.*)\/(status|statuses)\/(\d+)$/)) {
+      endpoint = 'https://publish.twitter.com/oembed';
+      opts['lang'] = hexo.config.language;
+    } else if (opts.url.match(/^http(s?):\/\/www\.youtube\.com\/watch\?v=[-\w]{11,}$/)) {
+      endpoint = 'http://www.youtube.com/oembed';
+    } else if (opts.url.match(/^http(s?):\/\/vimeo\.com\/\d+$/)) {
+      endpoint = 'http://vimeo.com/api/oembed.json';
+    } else if (opts.url.match(/^http(s?):\/\/www\.flickr\.com\/photos\/[-@\w]+\/\d+\/$/)) {
+      endpoint = 'http://www.flickr.com/services/oembed/';
+    } else if (opts.url.match(/^http(s?):\/\/(www\.)?instagram\.com\/p\/\w+\/$/)) {
+      endpoint = 'http://api.instagram.com/oembed';
+    } else if (opts.url.match(/^http(s?):\/\/gyazo\.com\/\w*$/)) {
+      endpoint = 'https://api.gyazo.com/api/oembed/';
+    } else if (opts.url.match(/^http(s?):\/\/codepen\.io\/[\/\w]*$/)) {
+      endpoint = 'http://codepen.io/api/oembed';
+    } else if (opts.url.match(/^http(s?):\/\/runkit\.com\/[\/\w]*$/)) {
+      endpoint = 'https://embed.runkit.com/oembed';
+    }
 
-  var endpoint = '';
-  if(opts.url.match(/^http(s?):\/\/twitter\.com\/(.*)\/(status|statuses)\/(\d+)$/)){
-    endpoint = 'https://publish.twitter.com/oembed';
-    opts["lang"] = hexo.config.language;
-  }else if(opts.url.match(/^http(s?):\/\/www\.youtube\.com\/watch\?v=[-\w]{11,}$/)){
-    endpoint = 'http://www.youtube.com/oembed';
-  }else if(opts.url.match(/^http(s?):\/\/vimeo\.com\/\d+$/)){
-    endpoint = 'http://vimeo.com/api/oembed.json';
-  }else if(opts.url.match(/^http(s?):\/\/www\.flickr\.com\/photos\/[-@\w]+\/\d+\/$/)){
-    endpoint = 'http://www.flickr.com/services/oembed/';
-  }else if(opts.url.match(/^http(s?):\/\/(www\.)?instagram\.com\/p\/\w+\/$/)){
-    endpoint = 'http://api.instagram.com/oembed';
-  }else if(opts.url.match(/^http(s?):\/\/gyazo\.com\/\w*$/)){
-    endpoint = 'https://api.gyazo.com/api/oembed/';
-  }else if(opts.url.match(/^http(s?):\/\/codepen\.io\/[\/\w]*$/)){
-    endpoint = 'http://codepen.io/api/oembed';
+    var getRequest = function(uri) {
+      return requestpromise({
+        uri: uri,
+        transform2xxOnly: true,
+        transform: function(body) {
+          return JSON.parse(body);
+        }
+      });
+    };
+
+    var req = endpoint + '?' + querystring.stringify(opts);
+    return getRequest(req)
+      .then(function(data) {
+        switch (data.type) {
+          case 'photo':
+            var alt = data.title ? ' alt="' + data.title + '"' : '';
+            return '<a href="' + opts['url'] + '"><img src="' + data.url + '"' + alt + '></a>';
+            break;
+          case 'video':
+          case 'rich':
+            return data.html;
+            break;
+          default:
+            return;
+            break;
+        }
+      })
+      .catch(function(err) {
+        console.log(err.statusCode + ': ' + req);
+      });
+  },
+  {
+    async: true
   }
-
-  var getRequest = function(uri){
-    return requestpromise({
-      uri: uri,
-      transform2xxOnly: true,
-      transform: function(body){
-        return JSON.parse(body);
-      }
-    });
-  }
-
-  var req = endpoint + '?' + querystring.stringify(opts);
-  return getRequest(req)
-    .then(function(data){
-      switch(data.type){
-        case 'photo':
-          var alt = data.title ? ' alt="'+data.title+'"' : '';
-          return '<a href="'+opts['url']+'"><img src="'+data.url+'"'+alt+'></a>';
-          break;
-        case 'video':
-        case 'rich':
-          return data.html;
-          break;
-        default:
-          return;
-          break;
-      }
-    })
-    .catch(function(err){
-      console.log(err.statusCode+': '+req);
-    });
-},{
-  async: true
-});
+);
